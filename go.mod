f1a28d153b0366b83a1a5695667bab86a744e0dd
yxz <550507808@qq.com>
2021-09-30 16:02:38 +0800

    grpc 新坑

--- go.mod
+++ go.mod
@@ -9,13 +9,11 @@ require (
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/json-iterator/go v1.1.11 // indirect
-	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	github.com/onsi/gomega v1.16.0 // indirect
+	github.com/tal-tech/go-zero v1.2.1
 	go.uber.org/zap v1.19.1
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+	google.golang.org/grpc v1.41.0
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
---
+++ grpcInterceptor/Interceptor.go
@@ -0,0 +1,35 @@
+package grpcInterceptor
+
+import (
+	"Ytool/codes"
+	"Ytool/log"
+	"context"
+	"github.com/tal-tech/go-zero/core/breaker"
+	"google.golang.org/grpc"
+	"path"
+)
+
+// 请求日志打印
+func ReqLogInterceptor() grpc.UnaryServerInterceptor {
+	interceptor := grpc.UnaryServerInterceptor(func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		log.Infof("grpc method:", info.FullMethod, ",requestBody is:", req)
+		resp, err = handler(ctx, req)
+		if err != nil {
+			log.Errorf("grpc method:", info.FullMethod, "Error,", err)
+		}
+		log.Infof("grpc method:", info.FullMethod, ",responseBody is:", resp)
+		return resp, err
+	})
+	return interceptor
+}
+
+func BreakerInterceptor(ctx context.Context, method string, req, reply interface{},
+	cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// 基于请求方法进行熔断
+	breakerName := path.Join(cc.Target(), method)
+	return breaker.DoWithAcceptable(breakerName, func() error {
+		// 真正发起调用
+		return invoker(ctx, method, req, reply, cc, opts...)
+		// codes.Acceptable判断哪种错误需要加入熔断错误计数
+	}, codes.Acceptable)
+}
