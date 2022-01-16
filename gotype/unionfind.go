e905d652a02da141dda5634ebca880c2be3e37d0
yxz <550507808@qq.com>
2022-01-16 21:57:24 +0800

    uniondfind

--- gotype/unionfind.go
+++ gotype/unionfind.go
@@ -1,53 +1,66 @@
 package gotype
 
-import "errors"
-
-type unionSet struct {
-	set []int
+type UnionFind struct {
+	root []int
+	size []int
 }
 
-func NewUnionSet(size int) *unionSet {
-	buf := make([]int, size)
+// New returns an initialized list of size
+func NewUnionFind(size int) *UnionFind {
+	return new(UnionFind).init(size)
+}
+
+// Constructor initializes root and size arrays
+func (uf *UnionFind) init(size int) *UnionFind {
+	uf = new(UnionFind)
+	uf.root = make([]int, size)
+	uf.size = make([]int, size)
+
 	for i := 0; i < size; i++ {
-		buf[i] = i // 初始时，所有节点均指向自己
+		uf.root[i] = i
+		uf.size[i] = 1
 	}
-	return &unionSet{set: buf}
+
+	return uf
 }
 
-func (set *unionSet) GetSize() int {
-	return len(set.set)
+// Union connects p and q by finding their roots and comparing their respective
+// size arrays to keep the tree flat
+func (uf *UnionFind) Union(p int, q int) {
+	qRoot := uf.Root(q)
+	pRoot := uf.Root(p)
+
+	if uf.size[qRoot] < uf.size[pRoot] {
+		uf.root[qRoot] = uf.root[pRoot]
+		uf.size[pRoot] += uf.size[qRoot]
+	} else {
+		uf.root[pRoot] = uf.root[qRoot]
+		uf.size[qRoot] += uf.size[pRoot]
+	}
 }
 
-func (set *unionSet) GetID(p int) (int, error) {
-	if p < 0 || p > len(set.set) {
-		return 0, errors.New(
-			"failed to get ID,index is illegal.")
+// Root or Find traverses each parent element while compressing the
+// levels to find the root element of p
+// If we attempt to access an element outside the array it returns -1
+func (uf *UnionFind) Root(p int) int {
+	if p > len(uf.root)-1 {
+		return -1
 	}
-	return set.set[p], nil
+
+	for uf.root[p] != p {
+		uf.root[p] = uf.root[uf.root[p]]
+		p = uf.root[p]
+	}
+
+	return p
 }
 
-func (set *unionSet) IsConnected(p, q int) (bool, error) {
-	if p < 0 || p > len(set.set) || q < 0 || q > len(set.set) {
-		return false, errors.New(
-			"failed to get ID,index is illegal.")
-	}
-	return set.set[p] == set.set[q], nil
+// Root or Find
+func (uf *UnionFind) Find(p int) int {
+	return uf.Root(p)
 }
 
-func (set *unionSet) Union(p, q int) error {
-	b, err := set.IsConnected(p, q)
-	if err != nil {
-		return err
-	}
-
-	if !b {
-		pID := set.set[p]
-		qID := set.set[q]
-		for k, v := range set.set {
-			if v == pID {
-				set.set[k] = qID
-			}
-		}
-	}
-	return nil
+// Check if items p,q are connected
+func (uf *UnionFind) IsConnected(p int, q int) bool {
+	return uf.Root(p) == uf.Root(q)
 }
--- gotype/unionfind_test.go
+++ gotype/unionfind_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestUnionSet(t *testing.T) {
-	set := NewUnionSet(10)
+	set := NewUnionFind(10)
 	set.Union(1, 2)
 	set.Union(1, 3)
 	set.Union(2, 3)
 	set.Union(2, 4)
-	is, err := set.IsConnected(1, 2)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(set.set)
+	is := set.IsConnected(1, 2)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//fmt.Println(set.set)
 	fmt.Println(is)
 
 }
