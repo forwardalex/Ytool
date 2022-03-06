a86b07ed7bba668a55f1eccb177a10fea8e6ac68
yxz <550507808@qq.com>
2022-03-06 12:07:22 +0800

    feat：cros

--- grpc/grpcproxy/grpcproxy.go
+++ grpc/grpcproxy/grpcproxy.go
@@ -61,9 +61,9 @@ func StartWithInterceptor(port int, prefix string, target string, interceptor Pr
 			Message: "success",
 			Payload: NullStruct{},
 		}
-
-		w.Header().Set("content-type", "text/json")
-		w.Header().Set("Access-Control-Allow-Origin", "*")                    //允许跨域
+		w.Header().Set("Access-Control-Allow-Origin", "*")                    //允许访问所有域
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type")        //header的类型
+		w.Header().Set("content-type", "application/json")                    //返回数据格式是json
 		if r.Header.Get("tencent-leakscan") == "TST(Tencent Security Team)" { // 过滤扫描
 			respByte, _ := json.Marshal(resp)
 			w.Write(respByte)
